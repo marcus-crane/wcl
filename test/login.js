9fc02d7b33c87743881e019886d68ab0c4f1c737
Marcus Crane <marcus@thingsima.de>
2017-03-07 15:05:24 +1300

    Added login tests

---
+++ test/login.js
@@ -0,0 +1,50 @@
+const chai = require('chai')
+const expect = chai.expect
+const env = require('dotenv').config()
+const login = require('../lib/login')
+const fetchDetails = require('../lib/fetchDetails')
+
+describe('login functions', function() {
+    it('should return a valid session for a real account', function() {
+        this.timeout(3000)
+        return login(process.env.WCL_CARD_NUMBER, process.env.WCL_LAST_NAME)
+        .then((userSession) => {
+            return fetchDetails(userSession)
+        })
+        .then((account) => {
+            expect(account).to.be.an('object')
+            expect(account.fees).to.contain('$')
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+    })
+
+    it('should fail to return details when provided with a bad login', function() {
+        this.timeout(3000)
+        return login('c12345678', 'lastname', function() {
+            return fetchDetails(userSession)
+        })
+        .then((account) => {
+            expect(account).to.not.be.an('object')
+            expect(account).to.have.lengthOf(42)
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+    })
+
+    it('should fail to return details when provided with a bad session', function() {
+        this.timeout(3000)
+        return login(process.env.WCL_CARD_NUMBER, process.env.WCL_LAST_NAME)
+        .then((userSession) => {
+            return fetchDetails('notarealcookie')
+        })
+        .then((account) => {
+            expect(account).to.be.empty
+        })
+        .catch((err) => {
+            console.error(err)
+        })
+    })
+})
