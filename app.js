38e9168ba654e92d7868ae1221dd5dec4f8147e4
Marcus Crane <marcus@thingsima.de>
2017-05-14 23:40:25 +1200

    Rewriting using async await

--- app.js
+++ app.js
@@ -1,49 +1,5 @@
-// const axios = require('axios')
-// const env = require('dotenv').config()
-// const viewAccount = require('./lib/viewAccount')
+const login = require('./lib/login')
+require('dotenv').config()
 
-// viewAccount('loans')
-// .then((loans) => {
-//     // Problem is that account content is loaded vs JS and so isn't included in the response
-//     console.log(loans)
-// })
-// .catch((err) => {
-//     console.error(err)
-// })
+login(process.env.CARD_NUMBER, process.env.SURNAME)
 
-const axios = require('axios')
-const cheerio = require('cheerio')
-const he = require('he')
-
-axios.get('http://www.wcl.govt.nz/carlweb/jsp/pipcharges.jsp', {
-    headers: {
-        Cookie: cookie
-    }
-})
-.then((res) => {
-    let $ = cheerio.load(res.data, { xmlMode: false })
-    let unfilteredCharges = $('script').text()
-    let loansRegex = /\s\[([^]+)\s]/gm
-    let itemsRegex = /{([\s\S]+?)}/gm
-    let keysRegex = /\w+(?=:)/g
-    //console.log(res)
-    let chargesArray = unfilteredCharges.match(loansRegex)[0]
-    chargesArray = he.decode(chargesArray)
-    let chargesObjects = chargesArray.match(itemsRegex)
-
-    for (let i = 0; i < chargesObjects.length; i++) {
-        chargesObjects[i] = chargesObjects[i].replace(keysRegex, function(match) {
-            if (match !== 'http') {
-                return `"${match}"`
-            } else {
-                return match
-            }
-        })
-    }
-
-    let books = JSON.parse('[' + chargesObjects + ']')
-    console.log(books[6])
-})
-.catch((err) => {
-    console.log(err)
-})
